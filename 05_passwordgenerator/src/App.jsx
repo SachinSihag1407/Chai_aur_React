99b675d8fb065c7497748331a550871901f506f6
sachinsihag <sachinsihag1407@gmail.com>
2025-05-24 10:38:49 +0530

    ready for deploy

--- 05_passwordgenerator/src/App.jsx
+++ 05_passwordgenerator/src/App.jsx
@@ -39,13 +39,13 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-80 h-40 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3 font-bold text-2xl'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4  ">
         <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 border-black"
             placeholder="Password"
             readOnly
             ref={passwordRef}
