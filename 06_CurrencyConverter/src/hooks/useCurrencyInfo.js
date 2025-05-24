dc3644ba9ab8ccc993bf079f7908113744e42bec
sachinsihag <sachinsihag1407@gmail.com>
2025-05-24 10:39:26 +0530

    ready for deploy

--- 06_CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 06_CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -3,9 +3,8 @@ import { useState, useEffect } from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({});
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/
-    currency-api@1/latest/currencies/${currency}.json`)
-            .then((res) => res.json)        // it converts the data into json format
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res) => res.json())        // it converts the data into json format
             .then((res) => (setData(res[currency])))   // yha pr ye  ho rha h h ki jo responsce aa rha h
         //  usme se settdata kaa use krke inr like dollar or currencies ki letter nikale ja  rhe h 
 
